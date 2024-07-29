2dc5cf8ef821c5ef61f5da70b392c5e1be68295a
chris7462 <chris7462@gmail.com>
2024-07-29 10:53:22 -0400

    Switch to SE3

--- include/iekf_localizer/iekf_localizer.hpp
+++ include/iekf_localizer/iekf_localizer.hpp
@@ -7,14 +7,15 @@
 
 // ROS header
 #include <rclcpp/rclcpp.hpp>
-#include <sensor_msgs/msg/imu.hpp>
+// #include <sensor_msgs/msg/imu.hpp>
 #include <sensor_msgs/msg/nav_sat_fix.hpp>
+#include <geometry_msgs/msg/twist_stamped.hpp>
 
 // Eigen header
 #include <Eigen/Core>
 
 // manif header
-#include <manif/SE_2_3.h>
+#include <manif/SE3.h>
 
 
 namespace iekf_localizer
@@ -23,8 +24,9 @@ namespace iekf_localizer
 class IEKFLocalizer : public rclcpp::Node
 {
 
-using Matrix9d = Eigen::Matrix<double, 9, 9>;
-using Array9d = Eigen::Array<double, 9, 1>;
+using Matrix6d = Eigen::Matrix<double, 6, 6>;
+using Vector6d = Eigen::Matrix<double, 6, 1>;
+using Array6d = Eigen::Array<double, 6, 1>;
 
 public:
   IEKFLocalizer();
@@ -33,44 +35,33 @@ public:
 private:
   double freq_;
 
-  void imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg);
+  // void imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg);
   void gps_callback(const sensor_msgs::msg::NavSatFix::SharedPtr msg);
+  void vel_callback(const geometry_msgs::msg::TwistStamped::SharedPtr msg);
 
-  rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub_;
+  // rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub_;
   rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr gps_sub_;
+  rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr vel_sub_;
   rclcpp::TimerBase::SharedPtr timer_;
 
-  std::queue<sensor_msgs::msg::Imu::SharedPtr> imu_buff_;
+  // std::queue<sensor_msgs::msg::Imu::SharedPtr> imu_buff_;
   std::queue<sensor_msgs::msg::NavSatFix::SharedPtr> gps_buff_;
+  std::queue<geometry_msgs::msg::TwistStamped::SharedPtr> vel_buff_;
 
   std::mutex mtx_;
 
   // State & Error covariance
-  manif::SE_2_3d X_;
-  Matrix9d P_;
+  manif::SE3d X_;
+  Matrix6d P_;
 
   // Control
-  manif::SE_2_3Tangentd u_;
-  Array9d u_sigmas_;
-  Matrix9d U_;
-
-  // Gravity in the world frame
-  const Eigen::Vector3d g_;
-
-  // some value that will be used throught the program
-  Eigen::Vector3d omega_prev_;
-  Eigen::Vector3d alpha_prev_;
-
-  // time
-  rclcpp::Time time_prev_;
-  bool init_;
+  manif::SE3Tangentd u_;
+  Vector6d u_noisy_;
+  Array6d u_sigmas_;
+  Matrix6d U_;
 
   // Declare the Jacobians of the motion wrt robot and control
-  manif::SE_2_3d::Jacobian F_;  // F_ = J_x_x_ + (J_x_u_ * J_u_x_)
-  manif::SE_2_3d::Jacobian J_x_x_;  // d(X * exp(u)) / dX
-  manif::SE_2_3d::Jacobian J_x_u_;  // d(X * exp(u)) / du
-  manif::SE_2_3d::Jacobian J_u_x_;  // du / dX, since u is a state-dependent vector
-  Eigen::Matrix<double, 3, 9> J_e_xi_;  // Jacobian
+  manif::SE3d::Jacobian J_x, J_u;
 
   void run_ekf();
 };
--- src/iekf_localizer.cpp
+++ src/iekf_localizer.cpp
@@ -9,29 +9,30 @@ namespace iekf_localizer
 {
 
 IEKFLocalizer::IEKFLocalizer()
-: Node("iekf_localizer_node"), freq_{40.0}, g_(0.0, 0.0, -9.80665),
-  omega_prev_(0.0, 0.0, 0.0), alpha_prev_(-g_), time_prev_(), init_{false}
+: Node("iekf_localizer_node"), freq_{40.0}
 {
   rclcpp::QoS qos(10);
 
-  imu_sub_ = this->create_subscription<sensor_msgs::msg::Imu>(
-    "kitti/oxts/imu_rotated", qos,
-    std::bind(&IEKFLocalizer::imu_callback, this, std::placeholders::_1));
+  // imu_sub_ = this->create_subscription<sensor_msgs::msg::Imu>(
+  //   "kitti/oxts/imu_rotated", qos,
+  //   std::bind(&IEKFLocalizer::imu_callback, this, std::placeholders::_1));
 
   gps_sub_ = this->create_subscription<sensor_msgs::msg::NavSatFix>(
     "kitti/oxts/gps_shifted", qos,
     std::bind(&IEKFLocalizer::gps_callback, this, std::placeholders::_1));
 
+  vel_sub_ = this->create_subscription<geometry_msgs::msg::TwistStamped>(
+    "kitti/oxts/gps/vel", qos,
+    std::bind(&IEKFLocalizer::vel_callback, this, std::placeholders::_1));
+
   // Initial the state and covariance.
   X_.setIdentity();
   P_.setZero();
-  P_.block<3, 3>(0, 0) = 0.001 * Eigen::Matrix3d::Identity();
-  P_.block<3, 3>(3, 3) = 0.01 * Eigen::Matrix3d::Identity();
-  P_.block<3, 3>(6, 6) = 0.001 * Eigen::Matrix3d::Identity();
 
   // Initial control
-  u_.setZero();
-  u_sigmas_ << 0.0, 0.0, 0.0, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01;
+  u_noisy_ << 5.0, 0.0, 0.0, 0.0, 0.0, 0.0;
+  u_ = u_noisy_;
+  u_sigmas_ << 0.1, 0.1, 0.1, 0.1, 0.1, 0.1;
   U_ = (u_sigmas_ * u_sigmas_).matrix().asDiagonal();
 
   timer_ = this->create_wall_timer(
@@ -39,12 +40,12 @@ IEKFLocalizer::IEKFLocalizer()
     std::bind(&IEKFLocalizer::run_ekf, this));
 }
 
-void IEKFLocalizer::imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
-{
-  std::lock_guard<std::mutex> lock(mtx_);
-  imu_buff_.push(msg);
-  RCLCPP_INFO(get_logger(), "Get IMU");
-}
+// void IEKFLocalizer::imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
+// {
+//   std::lock_guard<std::mutex> lock(mtx_);
+//   imu_buff_.push(msg);
+//   RCLCPP_INFO(get_logger(), "Get IMU");
+// }
 
 void IEKFLocalizer::gps_callback(const sensor_msgs::msg::NavSatFix::SharedPtr msg)
 {
@@ -53,110 +54,160 @@ void IEKFLocalizer::gps_callback(const sensor_msgs::msg::NavSatFix::SharedPtr ms
   RCLCPP_INFO(get_logger(), "Get GPS");
 }
 
+void IEKFLocalizer::vel_callback(const geometry_msgs::msg::TwistStamped::SharedPtr msg)
+{
+  std::lock_guard<std::mutex> lock(mtx_);
+  vel_buff_.push(msg);
+  RCLCPP_INFO(get_logger(), "Get Velocity");
+}
+
 void IEKFLocalizer::run_ekf()
 {
-  if (!init_) {
-    RCLCPP_INFO(get_logger(), "Init IEKF");
-    time_prev_ = rclcpp::Node::now();
-    init_ = true;
-  } else {
-    RCLCPP_INFO(get_logger(), "Running IEKF!");
-    rclcpp::Time time_current = rclcpp::Node::now();
+  RCLCPP_INFO(get_logger(), "Running IEKF!");
+  rclcpp::Time time_current = rclcpp::Node::now();
 
-    //// Run predict
-    // predict requires the Imu measurement
-    if (!imu_buff_.empty()) {
-      mtx_.lock();
-      if ((time_current - rclcpp::Time(imu_buff_.front()->header.stamp)).seconds() > 0.1) {  // time sync has problem
-        imu_buff_.pop();
-        RCLCPP_WARN(this->get_logger(), "Timestamp unaligned, please check your IMU data.");
-        mtx_.unlock();
-      } else {
-        auto msg = imu_buff_.front();
-        imu_buff_.pop();
-        mtx_.unlock();
+  ////// Run predict
+  //// predict requires the Imu measurement
+  //if (!imu_buff_.empty()) {
+  //  mtx_.lock();
+  //  if ((time_current - rclcpp::Time(imu_buff_.front()->header.stamp)).seconds() > 0.1) {  // time sync has problem
+  //    imu_buff_.pop();
+  //    RCLCPP_WARN(this->get_logger(), "Timestamp unaligned, please check your IMU data.");
+  //    mtx_.unlock();
+  //  } else {
+  //    auto msg = imu_buff_.front();
+  //    imu_buff_.pop();
+  //    mtx_.unlock();
 
-        alpha_prev_ << msg->linear_acceleration.x, msg->linear_acceleration.y, msg->linear_acceleration.z;
-        omega_prev_ << msg->angular_velocity.x, msg->angular_velocity.y, msg->angular_velocity.z;
-      }
+  //    alpha_prev_ << msg->linear_acceleration.x, msg->linear_acceleration.y, msg->linear_acceleration.z;
+  //    omega_prev_ << msg->angular_velocity.x, msg->angular_velocity.y, msg->angular_velocity.z;
+  //  }
+  //}
+
+  // RCLCPP_INFO_STREAM(get_logger(), "alpha prev: = " << alpha_prev_.transpose());
+  // RCLCPP_INFO_STREAM(get_logger(), "omega prev: = " << omega_prev_.transpose());
+
+  //// get current state
+  //auto R_k = X_.rotation();
+  //auto v_k = X_.linearVelocity();
+  //auto acc_k = alpha_prev_ + R_k.transpose() * g_;
+  //// RCLCPP_INFO_STREAM(get_logger(), "acc : = " << acc_k.transpose());
+
+  //// update previous time for next propgagation
+  //double dt = (time_current - time_prev_).seconds();
+  //time_prev_ = time_current;
+
+  //// input control
+  //Eigen::Vector3d accLin = (R_k.transpose() * v_k) * dt + 0.5 * acc_k * dt * dt;
+  //Eigen::Vector3d gLin = R_k.transpose() * g_ * dt;
+  //Eigen::Matrix3d accLinCross = manif::skew(accLin);
+  //Eigen::Matrix3d gCross = manif::skew(gLin);
+
+  //u_ << accLin, omega_prev_ * dt, acc_k * dt;
+
+  //// State prediction
+  //X_ = X_.plus(u_, J_x_x_, J_x_u_); // X * exp(u), with Jacobians
+
+  //// Prepare Jacobian of state-dependent control vector
+  //J_u_x_.setZero();
+  //J_u_x_.block<3, 3>(0, 3) = accLinCross;
+  //J_u_x_.block<3, 3>(0, 6) = Eigen::Matrix3d::Identity() * dt;
+  //J_u_x_.block<3, 3>(6, 3) = gCross;
+  //F_ = J_x_x_ + J_x_u_ * J_u_x_;  // chain rule for system model Jacobian
+  //P_ = F_ * P_ * F_.transpose() + J_x_u_ * U_ * J_x_u_.transpose();
+
+  // Run velocity update
+  if (!vel_buff_.empty()) {
+    mtx_.lock();
+    if ((time_current - rclcpp::Time(vel_buff_.front()->header.stamp)).seconds() > 0.1) {  // time sync has problem
+      vel_buff_.pop();
+      RCLCPP_WARN(this->get_logger(), "Timestamp unaligned, please check your Velocity data.");
+      mtx_.unlock();
+    } else {
+      auto msg = vel_buff_.front();
+      vel_buff_.pop();
+      mtx_.unlock();
+
+      u_noisy_ << msg->twist.linear.x, msg->twist.linear.y, msg->twist.linear.z,
+                  msg->twist.angular.x, msg->twist.angular.y, msg->twist.angular.z;
+
+      u_ = u_noisy_;
+
+    //VelMeasurement z;
+    //z.nu() = msg->twist.linear.x;
+
+    //// check velocity update successful?
+    //if (ekf_.update(vel_model_, z)) {
+    //  ekf_.wrapStateYaw();
+
+    //  // publish odom to base link TF
+    //  tf2::Vector3 t_current(s.x(), s.y(), alt_);
+    //  tf2::Quaternion q_current;
+    //  q_current.setRPY(roll_, pitch_, s.theta());
+    //  q_current.normalize();
+
+    //  geometry_msgs::msg::TransformStamped odom_base_link_tf;
+    //  odom_base_link_tf.header.stamp = rclcpp::Node::now();
+    //  odom_base_link_tf.header.frame_id = "odom";
+    //  odom_base_link_tf.child_frame_id = "base_link";
+    //  odom_base_link_tf.transform.translation = tf2::toMsg(t_current);
+    //  odom_base_link_tf.transform.rotation = tf2::toMsg(q_current);
+
+    //  // send the transformation
+    //  tf_broadcaster_->sendTransform(odom_base_link_tf);
+
+    //  // save the odom->base transform
+    //  tf2::fromMsg(odom_base_link_tf.transform, odom_base_link_trans_);
+    //} else {
+    //  RCLCPP_INFO(
+    //    get_logger(), "Measurement Velocity is over the threshold. Discard this measurement.");
+    //}
     }
+  }
 
-    // RCLCPP_INFO_STREAM(get_logger(), "alpha prev: = " << alpha_prev_.transpose());
-    // RCLCPP_INFO_STREAM(get_logger(), "omega prev: = " << omega_prev_.transpose());
 
-    // get current state
-    auto R_k = X_.rotation();
-    auto v_k = X_.linearVelocity();
-    auto acc_k = alpha_prev_ + R_k.transpose() * g_;
-    // RCLCPP_INFO_STREAM(get_logger(), "acc : = " << acc_k.transpose());
+  // Run gps update
+  if (!gps_buff_.empty()) {
+    mtx_.lock();
+    if ((time_current - rclcpp::Time(gps_buff_.front()->header.stamp)).seconds() > 0.1) {  // time sync has problem
+      gps_buff_.pop();
+      RCLCPP_WARN(get_logger(), "Timestamp unaligned, please check your GPS data.");
+      mtx_.unlock();
+    } else {
+      auto msg = gps_buff_.front();
+      gps_buff_.pop();
+      mtx_.unlock();
 
-    // update previous time for next propgagation
-    double dt = (time_current - time_prev_).seconds();
-    time_prev_ = time_current;
+    //// measurement
+    //Eigen::Vector3d y(msg->latitude , msg->longitude, msg->altitude);
 
-    // input control
-    Eigen::Vector3d accLin = (R_k.transpose() * v_k) * dt + 0.5 * acc_k * dt * dt;
-    Eigen::Vector3d gLin = R_k.transpose() * g_ * dt;
-    Eigen::Matrix3d accLinCross = manif::skew(accLin);
-    Eigen::Matrix3d gCross = manif::skew(gLin);
+    //Eigen::Vector3d e = X_.act(Eigen::Vector3d::Zero(), J_e_xi_);
+    //RCLCPP_INFO_STREAM(get_logger(), "X tran = " << X_.translation().transpose());
+    //RCLCPP_INFO_STREAM(get_logger(), "y = " << y.transpose());
+    //RCLCPP_INFO_STREAM(get_logger(), "e = " << e.transpose());
 
-    u_ << accLin, omega_prev_ * dt, acc_k * dt;
+    //// Innovation
+    //// z = y - e;
 
-    // State prediction
-    X_ = X_.plus(u_, J_x_x_, J_x_u_); // X * exp(u), with Jacobians
+    //// gps measurement
+    //GpsMeasurement z;
+    //z.x() = msg->latitude;
+    //z.y() = msg->longitude;
+    //alt_ = msg->altitude;
 
-    // Prepare Jacobian of state-dependent control vector
-    J_u_x_.setZero();
-    J_u_x_.block<3, 3>(0, 3) = accLinCross;
-    J_u_x_.block<3, 3>(0, 6) = Eigen::Matrix3d::Identity() * dt;
-    J_u_x_.block<3, 3>(6, 3) = gCross;
-    F_ = J_x_x_ + J_x_u_ * J_u_x_;  // chain rule for system model Jacobian
-    P_ = F_ * P_ * F_.transpose() + J_x_u_ * U_ * J_x_u_.transpose();
+    //// use the covariance that Gps provided.
+    //kalman::Covariance<GpsMeasurement> R = kalman::Covariance<GpsMeasurement>::Zero();
+    //R.diagonal() << msg->position_covariance.at(0), msg->position_covariance.at(4);
+    //gps_model_.setCovariance(R);
 
-    // Run gps update
-    if (!gps_buff_.empty()) {
-      mtx_.lock();
-      if ((time_current - rclcpp::Time(gps_buff_.front()->header.stamp)).seconds() > 0.1) {  // time sync has problem
-        gps_buff_.pop();
-        RCLCPP_WARN(get_logger(), "Timestamp unaligned, please check your GPS data.");
-        mtx_.unlock();
-      } else {
-        auto msg = gps_buff_.front();
-        gps_buff_.pop();
-        mtx_.unlock();
-
-        // measurement
-        Eigen::Vector3d y(msg->latitude , msg->longitude, msg->altitude);
-
-        Eigen::Vector3d e = X_.act(Eigen::Vector3d::Zero(), J_e_xi_);
-        RCLCPP_INFO_STREAM(get_logger(), "X tran = " << X_.translation().transpose());
-        RCLCPP_INFO_STREAM(get_logger(), "y = " << y.transpose());
-        RCLCPP_INFO_STREAM(get_logger(), "e = " << e.transpose());
-
-        // Innovation
-        // z = y - e;
-
-      //// gps measurement
-      //GpsMeasurement z;
-      //z.x() = msg->latitude;
-      //z.y() = msg->longitude;
-      //alt_ = msg->altitude;
-
-      //// use the covariance that Gps provided.
-      //kalman::Covariance<GpsMeasurement> R = kalman::Covariance<GpsMeasurement>::Zero();
-      //R.diagonal() << msg->position_covariance.at(0), msg->position_covariance.at(4);
-      //gps_model_.setCovariance(R);
-
-      //// check GPS update successful?
-      //if (ekf_.update(gps_model_, z)) {
-      //  ekf_.wrapStateYaw();
-      //} else {
-      //  RCLCPP_INFO(
-      //    get_logger(), "Measurement GPS is over the threshold. Discard this measurement.");
-      //}
-      }
+    //// check GPS update successful?
+    //if (ekf_.update(gps_model_, z)) {
+    //  ekf_.wrapStateYaw();
+    //} else {
+    //  RCLCPP_INFO(
+    //    get_logger(), "Measurement GPS is over the threshold. Discard this measurement.");
+    //}
     }
-
   }
 
 //// publish map to odom TF
