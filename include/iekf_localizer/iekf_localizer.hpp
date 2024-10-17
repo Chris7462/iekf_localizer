2da6662a0179f644f831086b6887a078ee1e6c41
chris7462 <chris7462@gmail.com>
2024-10-17 15:46:15 -0400

    Working on SE_2(3) model

--- include/iekf_localizer/iekf_localizer.hpp
+++ include/iekf_localizer/iekf_localizer.hpp
@@ -7,6 +7,7 @@
 
 // ROS header
 #include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/imu.hpp>
 #include <sensor_msgs/msg/nav_sat_fix.hpp>
 #include <geometry_msgs/msg/twist_stamped.hpp>
 #include <tf2_ros/transform_broadcaster.h>
@@ -15,7 +16,7 @@
 #include <Eigen/Core>
 
 // manif header
-#include <manif/SE3.h>
+#include <manif/SE_2_3.h>
 
 // local header
 #include <kitti_msgs/msg/geo_plane_point.hpp>
@@ -25,11 +26,10 @@ namespace iekf_localizer
 
 class IEKFLocalizer : public rclcpp::Node
 {
-  using Matrix6d = Eigen::Matrix<double, 6, 6>;
-  using Vector6d = Eigen::Matrix<double, 6, 1>;
-  using Array6d = Eigen::Array<double, 6, 1>;
-  using Matrix3x6d = Eigen::Matrix<double, 3, 6>;
-  using Matrix6x3d = Eigen::Matrix<double, 6, 3>;
+  using Matrix9d = Eigen::Matrix<double, 9, 9>;
+  using Array9d = Eigen::Array<double, 9, 1>;
+  using Matrix3x9d = Eigen::Matrix<double, 3, 9>;
+  using Matrix9x3d = Eigen::Matrix<double, 9, 3>;
 
 public:
   IEKFLocalizer();
@@ -39,9 +39,11 @@ private:
   double freq_;
   double dt_;
 
+  void imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg);
   void gps_callback(const kitti_msgs::msg::GeoPlanePoint::SharedPtr msg);
   void vel_callback(const geometry_msgs::msg::TwistStamped::SharedPtr msg);
 
+  rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub_;
   rclcpp::Subscription<kitti_msgs::msg::GeoPlanePoint>::SharedPtr gps_sub_;
   rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr vel_sub_;
   rclcpp::TimerBase::SharedPtr timer_;
@@ -49,22 +51,34 @@ private:
   std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
 
   std::queue<kitti_msgs::msg::GeoPlanePoint::SharedPtr> gps_buff_;
+  std::queue<geometry_msgs::msg::TwistStamped::SharedPtr> vel_buff_;
 
   std::mutex mtx_;
 
   // State & Error covariance
-  manif::SE3d X_;
-  Matrix6d P_;
+  manif::SE_2_3d X_;
+  Matrix9d P_R_;
 
   // Control
-  manif::SE3Tangentd u_;
-  Vector6d u_noisy_;
-  Array6d u_sigmas_;
-  Matrix6d Q_, I_;
+  manif::SE_2_3Tangentd u_noisy_;
+  Array9d u_sigmas_;
+  Matrix9d Q_, I_;
+
+  // Gravity in the world frame
+  Eigen::Vector3d g_;
+
+  // some value that will be used throught the program
+  Eigen::Vector3d omega_prev_;
+  Eigen::Vector3d alpha_prev_;
 
   // Declare the Jacobians of the motion wrt robot and control
-  manif::SE3d::Jacobian J_x_, J_u_;
-  Matrix3x6d H_;
+  manif::SE_2_3d::Jacobian F_;  // F_ = J_x_x_ + (J_x_u_ * J_u_x_)
+  manif::SE_2_3d::Jacobian W_;
+
+  manif::SE_2_3d::Jacobian AdX_, AdXinv_;  // Adjoints
+
+  // Declare the Jacobians of the observation wrt robot state
+  Matrix3x9d H_;  // Jacobian
   Eigen::Matrix3d V_;
 
   void run_ekf();
--- src/iekf_localizer.cpp
+++ src/iekf_localizer.cpp
@@ -17,6 +17,10 @@ IEKFLocalizer::IEKFLocalizer()
 {
   rclcpp::QoS qos(10);
 
+  imu_sub_ = this->create_subscription<sensor_msgs::msg::Imu>(
+    "kitti/vehicle/imu_local", qos,
+    std::bind(&IEKFLocalizer::imu_callback, this, std::placeholders::_1));
+
   gps_sub_ = this->create_subscription<kitti_msgs::msg::GeoPlanePoint>(
     "kitti/vehicle/gps_local", qos,
     std::bind(&IEKFLocalizer::gps_callback, this, std::placeholders::_1));
@@ -31,27 +35,43 @@ IEKFLocalizer::IEKFLocalizer()
   // Init the state
   std::vector<double> xyz = declare_parameter("init.xyz", std::vector<double>());
   std::vector<double> rpy = declare_parameter("init.rpy", std::vector<double>());
-  X_ = manif::SE3d(
-    Eigen::Vector3d(xyz.data()), manif::SO3d(rpy[0], rpy[1], rpy[2]));
+  std::vector<double> vxyz = declare_parameter("init.vxyz", std::vector<double>());
+  X_ = manif::SE_2_3d(
+    Eigen::Vector3d(xyz.data()),
+    manif::SO3d(rpy[0], rpy[1], rpy[2]),
+    Eigen::Vector3d(vxyz.data()));
 
-  // Init the state covariance
+  // Init the state covariance. Default is right-invariant
   std::vector<double> vec_P = declare_parameter("init.P", std::vector<double>());
-  P_ = Matrix6d(vec_P.data());
+  P_R_ = Matrix9d(vec_P.data());
 
   // no control inputs in the beginning
   u_noisy_.setZero();
   // Init control covariance
   std::vector<double> vec_sigmas = declare_parameter("u_sigmas", std::vector<double>());
-  u_sigmas_ = Array6d(vec_sigmas.data());
+  u_sigmas_ = Array9d(vec_sigmas.data());
   Q_ = (u_sigmas_ * u_sigmas_).matrix().asDiagonal();
 
   I_.setIdentity();
+
+  // gravity
+  std::vector<double> g = declare_parameter("g", std::vector<double>({0.0, 0.0, -9.80665}));
+  g_ = Eigen::Vector3d(g.data());
+  omega_prev_.setZero();
+  alpha_prev_ = -g_;
 
   timer_ = this->create_wall_timer(
     std::chrono::milliseconds(static_cast<int>(dt_ * 1000)),
     std::bind(&IEKFLocalizer::run_ekf, this));
 
   tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+}
+
+void IEKFLocalizer::imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
+{
+  std::lock_guard<std::mutex> lock(mtx_);
+  alpha_prev_ << msg->linear_acceleration.x, 0.0, 0.0;
+  omega_prev_ << 0.0, 0.0, msg->angular_velocity.z;
 }
 
 void IEKFLocalizer::gps_callback(const kitti_msgs::msg::GeoPlanePoint::SharedPtr msg)
@@ -63,70 +83,68 @@ void IEKFLocalizer::gps_callback(const kitti_msgs::msg::GeoPlanePoint::SharedPtr
 void IEKFLocalizer::vel_callback(const geometry_msgs::msg::TwistStamped::SharedPtr msg)
 {
   std::lock_guard<std::mutex> lock(mtx_);
-  u_noisy_ <<
-    msg->twist.linear.x, 0.0, 0.0,
-    0.0, 0.0, msg->twist.angular.z;
+  vel_buff_.push(msg);
 }
 
 void IEKFLocalizer::run_ekf()
 {
   RCLCPP_INFO_ONCE(get_logger(), "Running IEKF!");
   rclcpp::Time time_current = rclcpp::Node::now();
-  u_ = (u_noisy_ * dt_);
+//  u_ = (u_noisy_ * dt_);
 
-  // State estimation
-  X_ = X_.plus(u_, J_x_, J_u_);
-  P_ = J_x_ * P_ * J_x_.transpose() + J_u_ * Q_ * J_u_.transpose();
+//  // State estimation
+//  X_ = X_.plus(u_, J_x_, J_u_);
+//  P_ = J_x_ * P_ * J_x_.transpose() + J_u_ * Q_ * J_u_.transpose();
 
-  // Run gps update
-  if (!gps_buff_.empty()) {
-    mtx_.lock();
-    if ((time_current - rclcpp::Time(gps_buff_.front()->header.stamp)).seconds() > 0.1) { // time sync has problem
-      gps_buff_.pop();
-      RCLCPP_WARN(get_logger(), "Timestamp unaligned, please check your GPS data.");
-      mtx_.unlock();
-    } else {
-      auto msg = gps_buff_.front();
-      gps_buff_.pop();
-      mtx_.unlock();
+//  // Run gps update
+//  if (!gps_buff_.empty()) {
+//    mtx_.lock();
+//    if ((time_current - rclcpp::Time(gps_buff_.front()->header.stamp)).seconds() > 0.1) { // time sync has problem
+//      gps_buff_.pop();
+//      RCLCPP_WARN(get_logger(), "Timestamp unaligned, please check your GPS data.");
+//      mtx_.unlock();
+//    } else {
+//      auto msg = gps_buff_.front();
+//      gps_buff_.pop();
+//      mtx_.unlock();
 
-      // measurement
-      Eigen::Vector3d y(msg->local_coordinate.x, msg->local_coordinate.y, msg->local_coordinate.z);
-      Eigen::Matrix3d R(msg->position_covariance.data());
+//      // measurement
+//      Eigen::Vector3d y(msg->local_coordinate.x, msg->local_coordinate.y, msg->local_coordinate.z);
+//      Eigen::Matrix3d R(msg->position_covariance.data());
 
-      // innovation
-      Eigen::Vector3d z = X_.inverse().act(y);  // X.inverse().act(ybar) = 0
+//      // innovation
+//      Eigen::Vector3d z = X_.inverse().act(y);  // X.inverse().act(ybar) = 0
 
-      // Jacobians
-      H_.topLeftCorner<3, 3>() = Eigen::Matrix3d::Identity();
-      H_.topRightCorner<3, 1>() = Eigen::Vector3d::Zero();
-      V_ = X_.inverse().rotation();
+//      // Jacobians
+//      H_.topLeftCorner<3, 3>() = Eigen::Matrix3d::Identity();
+//      H_.topRightCorner<3, 1>() = Eigen::Vector3d::Zero();
+//      V_ = X_.inverse().rotation();
 
-      // innovation covariance
-      Eigen::Matrix3d S = H_ * P_ * H_.transpose() + V_ * R * V_.transpose();
+//      // innovation covariance
+//      Eigen::Matrix3d S = H_ * P_ * H_.transpose() + V_ * R * V_.transpose();
 
-      // Mahalanobis distance
-      // qchisq(0.95, df=3) = 7.814728
-      double D = z.transpose() * S.inverse() * z;
-      if (D < 7.814728) {
-        // Kalman gain
-        Matrix6x3d K = P_ * H_.transpose() * S.inverse();
+//      // Mahalanobis distance
+//      // qchisq(0.95, df=3) = 7.814728
+//      double D = z.transpose() * S.inverse() * z;
+//      if (D < 7.814728) {
+//        // Kalman gain
+//        Matrix6x3d K = P_ * H_.transpose() * S.inverse();
 
-        // Correction step
-        manif::SE3Tangentd dx = K * z;
+//        // Correction step
+//        manif::SE3Tangentd dx = K * z;
 
-        // Update
-        X_ = X_.plus(dx);
-        // P_ = (I_ - K * H_) * P_; // This will have numerial issue
-        P_ = (I_ - K * H_) * P_ * (I_ - K * H_).transpose()
-             + K * V_ * R * V_.transpose() * K.transpose();
-      } else {
-        RCLCPP_INFO(
-          get_logger(),
-          "The Mahalanobis dist = %f is greater than the threshold. No ESKF correction!", D);
-      }
-    }
-  }
+//        // Update
+//        X_ = X_.plus(dx);
+//        // P_ = (I_ - K * H_) * P_; // This will have numerial issue
+//        P_ = (I_ - K * H_) * P_ * (I_ - K * H_).transpose()
+//             + K * V_ * R * V_.transpose() * K.transpose();
+//      } else {
+//        RCLCPP_INFO(
+//          get_logger(),
+//          "The Mahalanobis dist = %f is greater than the threshold. No ESKF correction!", D);
+//      }
+//    }
+//  }
 
   // publish map to base link TF
   Eigen::Vector3d t_current(X_.translation());
