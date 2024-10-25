58ca05802c1d80241fbc60e4a710eea77cf51cdc
chris7462 <chris7462@gmail.com>
2024-10-24 21:53:10 -0400

    Working on SE_2_3 model

--- include/iekf_localizer/iekf_localizer.hpp
+++ include/iekf_localizer/iekf_localizer.hpp
@@ -7,15 +7,14 @@
 
 // ROS header
 #include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/imu.hpp>
 #include <sensor_msgs/msg/nav_sat_fix.hpp>
 #include <geometry_msgs/msg/twist_stamped.hpp>
 #include <tf2_ros/transform_broadcaster.hpp>
 
 // Eigen header
 #include <Eigen/Core>
-
-// manif header
-#include <manif/SE3.h>
+#include <unsupported/Eigen/MatrixFunctions>
 
 // local header
 #include <kitti_msgs/msg/geo_plane_point.hpp>
@@ -23,13 +22,15 @@
 namespace iekf_localizer
 {
 
+using Matrix5d = Eigen::Matrix<double, 5, 5>; // State matrix
+
 class IEKFLocalizer : public rclcpp::Node
 {
-  using Matrix6d = Eigen::Matrix<double, 6, 6>;
-  using Vector6d = Eigen::Matrix<double, 6, 1>;
-  using Array6d = Eigen::Array<double, 6, 1>;
-  using Matrix3x6d = Eigen::Matrix<double, 3, 6>;
-  using Matrix6x3d = Eigen::Matrix<double, 6, 3>;
+  using Matrix9d = Eigen::Matrix<double, 9, 9>; // Error covariance
+  using Array9d = Eigen::Array<double, 9, 1>;
+  using Vector9d = Eigen::Matrix<double, 9, 1>;
+  using Matrix3x9d = Eigen::Matrix<double, 3, 9>; // Jacobian
+  using Matrix9x3d = Eigen::Matrix<double, 9, 3>; // Kalman gain
 
 public:
   IEKFLocalizer();
@@ -39,9 +40,11 @@ private:
   double freq_;
   double dt_;
 
+  void imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg);
   void gps_callback(const kitti_msgs::msg::GeoPlanePoint::SharedPtr msg);
   void vel_callback(const geometry_msgs::msg::TwistStamped::SharedPtr msg);
 
+  rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub_;
   rclcpp::Subscription<kitti_msgs::msg::GeoPlanePoint>::SharedPtr gps_sub_;
   rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr vel_sub_;
   rclcpp::TimerBase::SharedPtr timer_;
@@ -49,25 +52,38 @@ private:
   std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
 
   std::queue<kitti_msgs::msg::GeoPlanePoint::SharedPtr> gps_buff_;
+  std::queue<geometry_msgs::msg::TwistStamped::SharedPtr> vel_buff_;
 
   std::mutex mtx_;
 
   // State & Error covariance
-  manif::SE3d X_;
-  Matrix6d P_;
+  Matrix5d X_;
+  Matrix9d P_;
 
   // Control
-  manif::SE3Tangentd u_;
-  Vector6d u_noisy_;
-  Array6d u_sigmas_;
-  Matrix6d Q_, I_;
+  Array9d u_sigmas_;
+  Matrix9d Q_, I_;
+
+  // Gravity in the world frame
+  Eigen::Vector3d g_;
+
+  // some value that will be used throught the program
+  Eigen::Vector3d omega_prev_;
+  Eigen::Vector3d alpha_prev_;
 
   // Declare the Jacobians of the motion wrt robot and control
-  manif::SE3d::Jacobian F_, W_; // F_ = J_x_, W_ = J_u_;
-  Matrix3x6d H_;
+  Matrix9d F_, W_;  // F = J_f_x and W = J_f_epsilon
+
+  Matrix3x9d H_;
   Eigen::Matrix3d V_;
 
   void run_ekf();
+
+  Eigen::Matrix3d skew(const Eigen::Vector3d & u);
+  Eigen::Matrix3d gamma0(const Eigen::Vector3d & phi);
+  Eigen::Matrix3d gamma1(const Eigen::Vector3d & phi);
+  Eigen::Matrix3d gamma2(const Eigen::Vector3d & phi);
+  Matrix5d makeTwist(const Vector9d & u);
 };
 
 } // namespace iekf_localizer
--- src/iekf_localizer.cpp
+++ src/iekf_localizer.cpp
@@ -17,6 +17,10 @@ IEKFLocalizer::IEKFLocalizer()
 {
   rclcpp::QoS qos(10);
 
+  imu_sub_ = this->create_subscription<sensor_msgs::msg::Imu>(
+    "kitti/vehicle/imu_local", qos,
+    std::bind(&IEKFLocalizer::imu_callback, this, std::placeholders::_1));
+
   gps_sub_ = this->create_subscription<kitti_msgs::msg::GeoPlanePoint>(
     "kitti/vehicle/gps_local", qos,
     std::bind(&IEKFLocalizer::gps_callback, this, std::placeholders::_1));
@@ -29,29 +33,48 @@ IEKFLocalizer::IEKFLocalizer()
   dt_ = 1.0 / freq_;
 
   // Init the state
-  std::vector<double> xyz = declare_parameter("init.xyz", std::vector<double>());
   std::vector<double> rpy = declare_parameter("init.rpy", std::vector<double>());
-  X_ = manif::SE3d(
-    Eigen::Vector3d(xyz.data()), manif::SO3d(rpy[0], rpy[1], rpy[2]));
+  std::vector<double> vxyz = declare_parameter("init.vxyz", std::vector<double>());
+  std::vector<double> xyz = declare_parameter("init.xyz", std::vector<double>());
+
+  X_.setIdentity();
+  X_.block<3, 3>(0, 0) = Eigen::Quaterniond(
+    Eigen::AngleAxisd(rpy[0], Eigen::Vector3d::UnitX()) *
+    Eigen::AngleAxisd(rpy[1], Eigen::Vector3d::UnitY()) *
+    Eigen::AngleAxisd(rpy[2], Eigen::Vector3d::UnitZ())).matrix();
+  X_.block<3, 1>(0, 3) = Eigen::Vector3d(vxyz.data());
+  X_.block<3, 1>(0, 4) = Eigen::Vector3d(xyz.data());
 
   // Init the state covariance
   std::vector<double> vec_P = declare_parameter("init.P", std::vector<double>());
-  P_ = Matrix6d(vec_P.data());
+  P_ = Matrix9d(vec_P.data());
 
-  // no control inputs in the beginning
-  u_noisy_.setZero();
   // Init control covariance
   std::vector<double> vec_sigmas = declare_parameter("u_sigmas", std::vector<double>());
-  u_sigmas_ = Array6d(vec_sigmas.data());
+  u_sigmas_ = Array9d(vec_sigmas.data());
   Q_ = (u_sigmas_ * u_sigmas_).matrix().asDiagonal();
 
   I_.setIdentity();
+
+  // gravity
+  std::vector<double> g = declare_parameter("g", std::vector<double>({0.0, 0.0, -9.80665}));
+  g_ = Eigen::Vector3d(g.data());
+
+  omega_prev_.setZero();
+  alpha_prev_ = -g_;
 
   timer_ = this->create_wall_timer(
     std::chrono::milliseconds(static_cast<int>(dt_ * 1000)),
     std::bind(&IEKFLocalizer::run_ekf, this));
 
   tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+}
+
+void IEKFLocalizer::imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
+{
+  std::lock_guard<std::mutex> lock(mtx_);
+  alpha_prev_ << msg->linear_acceleration.x, msg->linear_acceleration.y, msg->linear_acceleration.z;
+  omega_prev_ << msg->angular_velocity.x, msg->angular_velocity.y, msg->angular_velocity.z;
 }
 
 void IEKFLocalizer::gps_callback(const kitti_msgs::msg::GeoPlanePoint::SharedPtr msg)
@@ -63,19 +86,36 @@ void IEKFLocalizer::gps_callback(const kitti_msgs::msg::GeoPlanePoint::SharedPtr
 void IEKFLocalizer::vel_callback(const geometry_msgs::msg::TwistStamped::SharedPtr msg)
 {
   std::lock_guard<std::mutex> lock(mtx_);
-  u_noisy_ <<
-    msg->twist.linear.x, 0.0, 0.0,
-    0.0, 0.0, msg->twist.angular.z;
+  vel_buff_.push(msg);
 }
 
 void IEKFLocalizer::run_ekf()
 {
   RCLCPP_INFO_ONCE(get_logger(), "Running IEKF!");
   rclcpp::Time time_current = rclcpp::Node::now();
-  u_ = (u_noisy_ * dt_);
 
   // State estimation
-  X_ = X_.plus(u_, F_, W_);
+  // get current state
+  auto R_k = X_.block<3, 3>(0, 0);
+  auto v_k = X_.block<3, 1>(0, 3);
+  auto p_k = X_.block<3, 1>(0, 4);
+
+  X_.block<3, 3>(0, 0) = R_k * gamma0(omega_prev_ * dt_);
+  X_.block<3, 1>(0, 3) = v_k + R_k * gamma1(omega_prev_ * dt_) * alpha_prev_ * dt_ + g_ * dt_;
+  X_.block<3, 1>(0, 4) = p_k + v_k * dt_ + R_k * gamma2(omega_prev_ * dt_) * alpha_prev_ * dt_ * dt_  + 0.5 * g_ * dt_ * dt_;
+
+  // Prepare Jacobian of state-dependent control vector
+  Matrix9d A = Matrix9d::Zero();
+  A.block<3, 3>(0, 0) = -skew(omega_prev_);
+  A.block<3, 3>(3, 0) = -skew(alpha_prev_);
+  A.block<3, 3>(3, 3) = -skew(omega_prev_);
+  A.block<3, 3>(6, 3) = Eigen::Matrix3d::Identity();
+  A.block<3, 3>(6, 6) = -skew(omega_prev_);
+
+  // Left invariant Jacobians
+  F_ = (A * dt_).exp();
+  W_.setIdentity();
+
   P_ = F_ * P_ * F_.transpose() + W_ * Q_ * W_.transpose();
 
   // Run gps update
@@ -95,12 +135,13 @@ void IEKFLocalizer::run_ekf()
       Eigen::Matrix3d R(msg->position_covariance.data());
 
       // innovation
-      Eigen::Vector3d z = X_.inverse().act(y);  // X.inverse().act(ybar) = 0
+      auto X_inv = X_.inverse();
+      Eigen::Vector3d z = X_inv.block<3, 3>(0, 0) * y + X_inv.block<3, 1>(0, 4);
 
-      // Jacobians
-      H_.topLeftCorner<3, 3>() = Eigen::Matrix3d::Identity();
-      H_.topRightCorner<3, 1>() = Eigen::Vector3d::Zero();
-      V_ = X_.inverse().rotation();
+      // Left Jacobians
+      H_.setZero();
+      H_.topRightCorner<3, 3>() = Eigen::Matrix3d::Identity();
+      V_ = X_inv.block<3, 3>(0, 0);
 
       // innovation covariance
       Eigen::Matrix3d S = H_ * P_ * H_.transpose() + V_ * R * V_.transpose();
@@ -110,13 +151,14 @@ void IEKFLocalizer::run_ekf()
       double D = z.transpose() * S.inverse() * z;
       if (D < 7.814728) {
         // Kalman gain
-        Matrix6x3d K = P_ * H_.transpose() * S.inverse();
+        Matrix9x3d K = P_ * H_.transpose() * S.inverse();
 
         // Correction step
-        manif::SE3Tangentd dx = K * z;
+        Vector9d dx = K * z;
+        Matrix5d xi = makeTwist(dx);
 
         // Update
-        X_ = X_.plus(dx);
+        X_ = X_ * xi.exp();
         // P_ = (I_ - K * H_) * P_; // This will have numerial issue
         P_ = (I_ - K * H_) * P_ * (I_ - K * H_).transpose()
              + K * V_ * R * V_.transpose() * K.transpose();
@@ -129,8 +171,8 @@ void IEKFLocalizer::run_ekf()
   }
 
   // publish map to base link TF
-  Eigen::Vector3d t_current(X_.translation());
-  Eigen::Quaterniond q_current(X_.rotation());
+  Eigen::Vector3d t_current(X_.block<3, 1>(0, 4));
+  Eigen::Quaterniond q_current(X_.block<3, 3>(0, 0));
   q_current.normalize();
 
   geometry_msgs::msg::TransformStamped map_base_link_tf;
@@ -142,6 +184,58 @@ void IEKFLocalizer::run_ekf()
 
   // send the transformation
   tf_broadcaster_->sendTransform(map_base_link_tf);
+}
+
+Eigen::Matrix3d IEKFLocalizer::skew(const Eigen::Vector3d & u)
+{
+  return (Eigen::Matrix3d() <<
+    0.0, -u(2), u(1),
+    u(2), 0.0, -u(0),
+    -u(1), u(0), 0.0).finished();
+}
+
+
+Eigen::Matrix3d IEKFLocalizer::gamma0(const Eigen::Vector3d & phi)
+{
+  const double norm_phi = phi.norm();
+  if (norm_phi > 1e-8) {
+    auto skew_phi = skew(phi);
+    return (Eigen::Matrix3d::Identity() + sin(norm_phi) / norm_phi * skew_phi +
+      (1 - cos(norm_phi)) / pow(norm_phi, 2) * skew_phi * skew_phi);
+  }
+  return Eigen::Matrix3d::Identity();
+}
+
+Eigen::Matrix3d IEKFLocalizer::gamma1(const Eigen::Vector3d & phi)
+{
+  const double norm_phi = phi.norm();
+  if (norm_phi > 1e-8) {
+    auto skew_phi = skew(phi);
+    return (Eigen::Matrix3d::Identity() + (1 - cos(norm_phi)) / pow(norm_phi, 2) * skew_phi +
+      (norm_phi - sin(norm_phi)) / pow(norm_phi, 3) * skew_phi * skew_phi);
+  }
+  return Eigen::Matrix3d::Identity();
+}
+
+Eigen::Matrix3d IEKFLocalizer::gamma2(const Eigen::Vector3d & phi)
+{
+  const double norm_phi = phi.norm();
+  if (norm_phi > 1e-8) {
+    auto skew_phi = skew(phi);
+    return (0.5 * Eigen::Matrix3d::Identity() +
+      (norm_phi - sin(norm_phi)) / pow(norm_phi, 3) * skew_phi +
+      (pow(norm_phi, 2) + 2 * cos(norm_phi) - 2) / (2 * pow(norm_phi, 4)) * skew_phi * skew_phi);
+  }
+  return 0.5 * Eigen::Matrix3d::Identity();
+}
+
+Matrix5d IEKFLocalizer::makeTwist(const Vector9d & u)
+{
+  Matrix5d twist = Matrix5d::Zero();
+  twist.block<3, 3>(0, 0) = skew(u.block<3, 1>(0, 0));
+  twist.block<3, 1>(0, 3) = u.block<3, 1>(3, 0);
+  twist.block<3, 1>(0, 4) = u.block<3, 1>(6, 0);
+  return twist;
 }
 
 }  // namespace iekf_localizer
