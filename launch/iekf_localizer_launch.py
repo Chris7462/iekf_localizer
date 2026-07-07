82698c4020c3d9cebf67584cc15963200fcdf9e5
Yi-Chen Zhang <chris7462@gmail.com>
2026-07-07 11:25:22 -0400

    bump to lyrical (#3)

--- launch/iekf_localizer_launch.py
+++ launch/iekf_localizer_launch.py
@@ -3,21 +3,34 @@ from os.path import join
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    declare_use_sim_time = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',  # or 'true' if appropriate
+        description='Use simulation time'
+    )
+
     params = join(
-        get_package_share_directory('iekf_localizer'), 'params', 'iekf_localizer.yaml'
+        get_package_share_directory('iekf_localizer'), 'param',
+        'iekf_localizer.yaml'
     )
 
     iekf_localizer_node = Node(
         package='iekf_localizer',
         executable='iekf_localizer_node',
         name='iekf_localizer_node',
-        parameters=[params]
+        parameters=[
+            params,
+            {'use_sim_time': LaunchConfiguration('use_sim_time')}
+        ]
     )
 
     return LaunchDescription([
+        declare_use_sim_time,
         iekf_localizer_node
     ])
